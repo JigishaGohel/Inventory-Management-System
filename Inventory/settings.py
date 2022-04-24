ccf5a5c3a0fd2d54b9373b794fe6412dee06d3af
JigishaGohel <102803312+JigishaGohel@users.noreply.github.com>
2022-04-24 16:38:09 +0530

    update 1.0.4

--- Inventory/settings.py
+++ Inventory/settings.py
@@ -141,3 +141,14 @@ MEDIA_URL = '/media/'
 LOGIN_REDIRECT_URL= 'dashboard-index'
 
 LOGIN_URL = 'user-login'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+
+EMAIL_PORT = 587
+
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = 'jig.inventory@gmail.com'
+
+EMAIL_HOST_PASSWORD = 'project789'
